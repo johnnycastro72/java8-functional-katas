1c4a5fe5ebdaea71ed8d0df01ab95be0239638e5
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-25 21:54:01 -0500

    Exercise 6
    Exercise 6 solved and tested

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -4,6 +4,7 @@ import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -13,7 +14,12 @@ import java.util.List;
 public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
-
-        return "someUrl";
+        return movies.stream().map(movie -> movie.getBoxarts())
+                .map(boxArt -> boxArt.stream().reduce(((boxArt1, boxArt2) ->
+                        (boxArt1.getHeight()*boxArt1.getWidth()) > (boxArt2.getHeight()*boxArt2.getWidth()) ?
+                                boxArt1 :
+                                boxArt2)))
+                .map(boxArt -> boxArt.get().getUrl())
+                .collect(Collectors.toUnmodifiableList()).get(movies.size()-1);
     }
 }
